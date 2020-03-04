727f1e84d7cc8eba1452ecf03aca75dbee4184ac
Josh Saint Jacque <joshsaintjacque@gmail.com>
2020-03-03 23:07:38 -0800

    task(twists): add urls for some twists

--- data/twists.js
+++ data/twists.js
@@ -9,8 +9,14 @@ const DEFAULT_TWISTS = [
     name: 'using the Pomodoro Technique',
     url: 'https://medium.com/@mr_mig_by/pomodoro-for-programmers-d6568dd1e6fc',
   },
-  { name: 'with Domain-Driven Design (DDD)' },
-  { name: 'with Test-Driven Development (TDD)' },
+  {
+    name: 'with Domain-Driven Design (DDD)',
+    url: 'https://medium.com/the-coding-matrix/ddd-101-the-5-minute-tour-7a3037cf53b8',
+  },
+  {
+    name: 'with Test-Driven Development (TDD)',
+    url: 'https://airbrake.io/blog/sdlc/test-driven-development',
+  },
   { name: 'without dependencies' },
 ];
 
