235164864439117aaa8ddb4926ad3fd18fead05a
Josh Saint Jacque <joshsaintjacque@gmail.com>
2020-03-03 23:03:30 -0800

    feat(twists): add pomodoro technique

--- data/twists.js
+++ data/twists.js
@@ -5,6 +5,10 @@ const DEFAULT_TWISTS = [
   { name: 'in 4 hours or less' },
   { name: 'in less than 1kb' },
   { name: 'in less than 5kb' },
+  {
+    name: 'using the Pomodoro Technique',
+    url: 'https://medium.com/@mr_mig_by/pomodoro-for-programmers-d6568dd1e6fc',
+  },
   { name: 'with Domain-Driven Design (DDD)' },
   { name: 'with Test-Driven Development (TDD)' },
   { name: 'without dependencies' },
