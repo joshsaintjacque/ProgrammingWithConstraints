ac4a60a321f44d52fd66c43d35e420544215e732
Josh Saint Jacque <josh.saint.jacque@voom.flights>
2020-03-08 15:07:19 -0700

    task(twists): change name format

--- data/twists.js
+++ data/twists.js
@@ -1,25 +1,26 @@
 const DEFAULT_TWISTS = [
-  { name: 'any way you want' },
-  { name: 'in 1 hour or less' },
-  { name: 'in 2 hours or less' },
-  { name: 'in 4 hours or less' },
-  { name: 'in less than 1kb' },
-  { name: 'in less than 5kb' },
-  { name: 'with 100% Test Coverage' },
-  { name: 'with Mutation Testing' },
+  { name: "any way you want" },
+  { name: "a 1 hour time limit" },
+  { name: "a 2 hour time limit" },
+  { name: "a 4 hour time limit" },
+  { name: "a max of 1kb" },
+  { name: "a max of 5kb" },
+  { name: "100% Test Coverage" },
+  { name: "Mutation Testing" },
   {
-    name: 'using the Pomodoro Technique',
-    url: 'https://medium.com/@mr_mig_by/pomodoro-for-programmers-d6568dd1e6fc',
+    name: "the Pomodoro Technique",
+    url: "https://medium.com/@mr_mig_by/pomodoro-for-programmers-d6568dd1e6fc"
   },
   {
-    name: 'with Domain-Driven Design (DDD)',
-    url: 'https://medium.com/the-coding-matrix/ddd-101-the-5-minute-tour-7a3037cf53b8',
+    name: "Domain-Driven Design (DDD)",
+    url:
+      "https://medium.com/the-coding-matrix/ddd-101-the-5-minute-tour-7a3037cf53b8"
   },
   {
-    name: 'with Test-Driven Development (TDD)',
-    url: 'https://airbrake.io/blog/sdlc/test-driven-development',
+    name: "Test-Driven Development (TDD)",
+    url: "https://airbrake.io/blog/sdlc/test-driven-development"
   },
-  { name: 'without dependencies' },
+  { name: "no dependencies" }
 ];
 
 export default DEFAULT_TWISTS;
